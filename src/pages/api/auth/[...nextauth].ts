86d3990c7b2a46dd9dd89d7bfca54eba14ffbba5
rami chabchoub <ramichabchoub.rc@gmail.com>
2023-05-25 17:06:03 +0100

    added auth0 provider

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -2,8 +2,9 @@ import NextAuth from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
 import GitHubProvider from 'next-auth/providers/github'
 import DiscordProvider from 'next-auth/providers/discord'
-// import TwitterProvider from 'next-auth/providers/twitter'
 import FacebookProvider from 'next-auth/providers/facebook'
+// import TwitterProvider from 'next-auth/providers/twitter'
+import Auth0Provider from 'next-auth/providers/auth0'
 
 export default NextAuth({
   providers: [
@@ -27,6 +28,11 @@ export default NextAuth({
     FacebookProvider({
       clientId: process.env.FACEBOOK_ID as string,
       clientSecret: process.env.FACEBOOK_SECRET as string,
+    }),
+    Auth0Provider({
+      clientId: process.env.AUTH0_ID as string,
+      clientSecret: process.env.AUTH0_SECRET as string,
+      issuer: process.env.AUTH0_ISSUER as string,
     }),
     // Passwordless / email sign in
   ],
